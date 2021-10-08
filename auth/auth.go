0a5e74a51ed8d34d37c8ace1274aa6373f343868
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2021-10-08 19:51:00 -0300

    Simplifica VerifyIdToken

--- auth/auth.go
+++ auth/auth.go
@@ -4,12 +4,21 @@ import (
 	"context"
 	"errors"
 	"os"
+	"strings"
 
 	"google.golang.org/api/idtoken"
 	"poscomp-simulator.com/backend/models"
 )
 
-func VerifyIdToken(idToken string) (models.Usuario, error) {
+func VerifyIdToken(auth string) (models.Usuario, error) {
+
+	authorization := strings.Fields(auth)
+
+	if len(authorization) != 2 || authorization[0] != "Bearer" {
+		return models.Usuario{}, errors.New("Formato de autenticação incorreto.")
+	}
+
+	idToken := authorization[1]
 
 	if idToken == os.Getenv("DUMMY_TOKEN") {
 		u := models.Usuario{}
