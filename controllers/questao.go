65df146eea55f023712c47b50dbfdedbaa6466fa
Itallo Silva <itallo.silva@ccc.ufcg.edu.br>
2021-10-09 20:27:51 -0300

    Implementa GET e POST /questao/

--- controllers/questao.go
+++ controllers/questao.go
@@ -1,16 +1,87 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"poscomp-simulator.com/backend/auth"
+	"poscomp-simulator.com/backend/models"
+	"poscomp-simulator.com/backend/utils"
 )
 
-func (a *App) GetQuestoes(w http.ResponseWriter, r *http.Request) {}
+func (a *App) GetQuestoes(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	var batch models.BatchQuestoes
+	if val, ok := r.Form["anos"]; ok {
+		batch.Filtros.Anos = make([]int, len(val))
+		for e, v := range val {
+			i, err := strconv.Atoi(v)
+
+			if err != nil {
+				utils.RespondWithError(w, http.StatusBadRequest, "Campo anos mal formatado")
+			}
+
+			batch.Filtros.Anos[e] = i
+		}
+	}
+
+	if val, ok := r.Form["areas"]; ok {
+		batch.Filtros.Areas = val
+	}
+
+	if _, ok := r.Form["sinalizadas"]; ok {
+		batch.Filtros.Sinalizadas = true
+	}
+
+	if err := batch.Get(a.DB); err != nil {
+		fmt.Println(err)
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, batch)
+
+}
 
 func (a *App) GetQSumario(w http.ResponseWriter, r *http.Request) {}
 
 func (a *App) GetErrosQuestao(w http.ResponseWriter, r *http.Request) {}
 
-func (a *App) CreateQuestao(w http.ResponseWriter, r *http.Request) {}
+func (a *App) CreateQuestao(w http.ResponseWriter, r *http.Request) {
+
+	user, err := auth.VerifyIdToken(r.Header.Get("Authorization"))
+	if err != nil {
+		utils.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	if err = user.Get(a.DB); err != nil {
+		utils.RespondWithError(w, http.StatusNotFound, err.Error())
+		return
+	}
+
+	if user.NivelAcesso < 1 {
+		utils.RespondWithError(w, http.StatusUnauthorized, "Usuário não autorizado a realizar a operação.")
+		return
+	}
+
+	var q models.Questao
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&q); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := q.Create(a.DB); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
 
 func (a *App) ReportQuestao(w http.ResponseWriter, r *http.Request) {}
 
--- models/questao.go
+++ models/questao.go
@@ -3,6 +3,8 @@ package models
 import (
 	"database/sql"
 	"errors"
+	"strconv"
+	"strings"
 )
 
 type Questao struct {
@@ -19,8 +21,14 @@ type Questao struct {
 }
 
 type BatchQuestoes struct {
-	Questoes []Questao              `json:"questoes,omitempty"`
-	Filtros  map[string]interface{} `json:"-"`
+	Questoes []Questao     `json:"questoes"`
+	Filtros  FiltroQuestao `json:"-"`
+}
+
+type FiltroQuestao struct {
+	Anos        []int
+	Areas       []string
+	Sinalizadas bool
 }
 
 type ErrosQuestao struct {
@@ -43,7 +51,111 @@ type SumarioQuestoes struct {
 }
 
 func (bq *BatchQuestoes) Get(db *sql.DB) error {
-	return errors.New("Not implemented")
+
+	bq.Questoes = []Questao{}
+	ids := []interface{}{}
+	map_id_questao := map[int]Questao{}
+
+	queryString, args := bq.mountFilterQuery()
+	rows, err := db.Query(queryString, args...)
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		q := Questao{}
+		err := rows.Scan(&q.ID, &q.Ano, &q.Numero, &q.Area, &q.Subarea,
+			&q.Alternativas[0], &q.Alternativas[1], &q.Alternativas[2],
+			&q.Alternativas[3], &q.Alternativas[4], &q.Resposta, &q.Sinalizada)
+
+		if err != nil {
+			return err
+		}
+
+		q.ImagensQuestao.A = []string{}
+		q.ImagensQuestao.B = []string{}
+		q.ImagensQuestao.C = []string{}
+		q.ImagensQuestao.D = []string{}
+		q.ImagensQuestao.E = []string{}
+		q.ImagensQuestao.Enunciado = []string{}
+
+		ids = append(ids, q.ID)
+		map_id_questao[q.ID] = q
+	}
+
+	if len(ids) > 0 {
+
+		seps := map[bool]string{true: ",$", false: "$"}
+
+		auxQuery := " WHERE id_questao IN ("
+		for i := 0; i < len(ids); i++ {
+			auxQuery += seps[i > 0] + strconv.Itoa(i+1)
+		}
+		auxQuery += ")"
+
+		rows, err = db.Query("SELECT id_questao, texto FROM enunciado_questao"+auxQuery, ids...)
+		if err != nil {
+			return err
+		}
+
+		for rows.Next() {
+			var (
+				id    int
+				texto string
+			)
+
+			rows.Scan(&id, &texto)
+
+			q := map_id_questao[id]
+			q.Enunciado = append(q.Enunciado, texto)
+			map_id_questao[q.ID] = q
+
+		}
+
+		rows, err = db.Query("SELECT * FROM imagem_questao"+auxQuery, ids...)
+		if err != nil {
+			return err
+		}
+
+		for rows.Next() {
+			var (
+				id   int
+				tipo string
+				url  string
+			)
+
+			rows.Scan(&id, &tipo, &url)
+
+			q := map_id_questao[id]
+
+			switch tipo {
+			case "A":
+				q.ImagensQuestao.A = append(q.ImagensQuestao.A, url)
+			case "B":
+				q.ImagensQuestao.B = append(q.ImagensQuestao.B, url)
+			case "C":
+				q.ImagensQuestao.C = append(q.ImagensQuestao.C, url)
+			case "D":
+				q.ImagensQuestao.D = append(q.ImagensQuestao.D, url)
+			case "E":
+				q.ImagensQuestao.E = append(q.ImagensQuestao.E, url)
+			default:
+				q.ImagensQuestao.Enunciado = append(q.ImagensQuestao.Enunciado, url)
+			}
+
+			map_id_questao[q.ID] = q
+
+		}
+
+		for _, q := range map_id_questao {
+			bq.Questoes = append(bq.Questoes, q)
+		}
+
+	}
+
+	return nil
 }
 
 func (sq *SumarioQuestoes) Get(db *sql.DB) error {
@@ -55,7 +167,70 @@ func (q *Questao) GetErros(db *sql.DB) error {
 }
 
 func (q *Questao) Create(db *sql.DB) error {
-	return errors.New("Not implemented")
+
+	if err := db.QueryRow("SELECT id FROM questao WHERE ano = $1 AND numero = $2", q.Ano, q.Numero).Scan(&q.ID); err == nil {
+		return errors.New("Questão já foi adicionada.")
+	}
+
+	var queries = [3]string{
+		`INSERT INTO questao(ano, numero, area, subarea, alternativa_a, alternativa_b, alternativa_c, alternativa_d, alternativa_e, gabarito)
+		VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+		RETURNING id
+		`,
+		"INSERT INTO enunciado_questao(id_questao, ordem, texto) VALUES($1, $2, $3)",
+		"INSERT INTO imagem_questao(id_questao, tipo, url_img) VALUES($1, $2, $3)",
+	}
+
+	if err := db.QueryRow(queries[0], q.Ano, q.Numero, q.Area,
+		q.Subarea, q.Alternativas[0], q.Alternativas[1],
+		q.Alternativas[2], q.Alternativas[3],
+		q.Alternativas[4], q.Resposta).Scan(&q.ID); err != nil {
+		return errors.New("Questão não pode ser criada.")
+	}
+
+	for e, v := range q.Enunciado {
+		if _, err := db.Exec(queries[1], q.ID, e, v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	for _, v := range q.ImagensQuestao.Enunciado {
+		if _, err := db.Exec(queries[2], q.ID, "", v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	for _, v := range q.ImagensQuestao.A {
+		if _, err := db.Exec(queries[2], q.ID, "A", v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	for _, v := range q.ImagensQuestao.B {
+		if _, err := db.Exec(queries[2], q.ID, "B", v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	for _, v := range q.ImagensQuestao.C {
+		if _, err := db.Exec(queries[2], q.ID, "C", v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	for _, v := range q.ImagensQuestao.D {
+		if _, err := db.Exec(queries[2], q.ID, "D", v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	for _, v := range q.ImagensQuestao.E {
+		if _, err := db.Exec(queries[2], q.ID, "E", v); err != nil {
+			return errors.New("Questão não pode ser criada.")
+		}
+	}
+
+	return nil
 }
 
 func (q *Questao) Report(db *sql.DB) error {
@@ -68,4 +243,59 @@ func (q *Questao) Update(db *sql.DB) error {
 
 func (q *Questao) Delete(db *sql.DB) error {
 	return errors.New("Not implemented")
+}
+
+func (bq *BatchQuestoes) mountFilterQuery() (string, []interface{}) {
+
+	seps := map[bool]string{true: ",$", false: "$"}
+
+	max_len := len(bq.Filtros.Anos) + len(bq.Filtros.Areas)
+
+	args, ind_args, ind_query := make([]interface{}, max_len), 0, 1
+	queries := []string{"SELECT * FROM questao"}
+
+	if len(bq.Filtros.Anos) > 0 {
+
+		queryFilter := "ano IN ("
+
+		for i, v := range bq.Filtros.Anos {
+			args[ind_args] = v
+			ind_args++
+			queryFilter += seps[i > 0] + strconv.Itoa(ind_args)
+		}
+
+		queryFilter += ")"
+		queries = append(queries, queryFilter)
+		ind_query++
+
+	}
+
+	if len(bq.Filtros.Areas) > 0 {
+
+		queryFilter := "area IN ("
+
+		for i, v := range bq.Filtros.Areas {
+			args[ind_args] = v
+			ind_args++
+			queryFilter += seps[i > 0] + strconv.Itoa(ind_args)
+		}
+
+		queryFilter += ")"
+		queries = append(queries, queryFilter)
+		ind_query++
+
+	}
+
+	if bq.Filtros.Sinalizadas {
+		queries = append(queries, "sinalizada = true")
+		ind_query++
+	}
+
+	if len(queries) > 1 {
+		queries[0] += " WHERE "
+		queries[0] += strings.Join(queries[1:], " AND ")
+	}
+
+	return queries[0] + " ORDER BY id", args
+
 }
